dcbb4d0e9af1e314d31463042f292b0f87b6d476
bacf <brunoagustincarusofassa@gmail.com>
2024-04-13 23:47:24 -0300

    log messages from post -> req.body

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.post('*', async (req, res) => {
+  console.log(req.body);
   res.send('hello post');
 });
 app.get('*', async (req, res) => {
@@ -15,3 +16,7 @@ app.listen(PORT, function (error) {
   if (error) console.log(error);
   console.log('Server listening on Port', PORT);
 });
+
+// https://api.telegram.org/bot6725242290:AAGbYYi3_t8WncBSWGuEGmp0dqTypLaVPnU/
+
+// BOT_TOKEN: 6725242290:AAGbYYi3_t8WncBSWGuEGmp0dqTypLaVPnU
